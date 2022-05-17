bf0da84a2a960a8ca53416f5baaabf3a29146755
VictorColominaAnoll <victorcolominanoll@gmail.com>
2022-05-17 14:55:12 +0200

    If I click the two cards, are hidden

--- client/src/cardGame/CardGame.js
+++ client/src/cardGame/CardGame.js
@@ -3,16 +3,23 @@ import { useState } from "react"
 export function CardGame() {
 
     const [isActive, setIsActive] = useState(false);
+    const [deleted, setDeleted] = useState(false)
 
     return (
         <>
             <h1>Card game</h1>
             <div>
-                <div>
-                    <span data-testid="card-1" className={isActive ? "cards selected" : "cards"} onClick={() => setIsActive(!isActive)}>❤️</span>
+                <div className={deleted ? "hide" : ""}>
+                    <span data-testid="card-1" className={isActive ? "cards selected" : "cards"} onClick={() => {
+                        setIsActive(!isActive);
+                    }}>❤️</span>
                 </div>
-                <div>
-                    <span data-testid="card-2" className={isActive ? "cards selected" : "cards"} onClick={() => setIsActive(!isActive)}>❤️</span>
+                <div className={deleted ? "hide" : ""}>
+                    <span data-testid="card-2" className={isActive ? "cards selected" : "cards"} onClick={() => {
+                        setIsActive(!isActive);
+                        setDeleted(true)
+
+                    }}>❤️</span>
                 </div>
             </div>
         </>
